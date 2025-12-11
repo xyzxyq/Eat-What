3621eead6b8d5164707322d8c7605da1bbb80132
xyz <37220232203927@stu.xmu.edu.cn>
2025-12-11 20:18:22 +0800

    fix

--- src/app/api/user/couple/route.ts
+++ src/app/api/user/couple/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 import { prisma } from '@/lib/prisma'
 import { getSessionFromRequest } from '@/lib/auth'
 
